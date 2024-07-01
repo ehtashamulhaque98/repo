850985b8e1e7c616d50d5caf6a3b921df570501d
ehtashamul98 <162790649+ehtashamul98@users.noreply.github.com>
2024-07-01 21:04:01 +0530

    Create Program 1.cs
    ok

---
+++ C# Questpond/Program 1.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome");
+        Console.WriteLine("Ehtashamul");
+
+
+        int myAge = 25;
+        int votingAge = 18;
+
+        int myInt = 9;
+        double myDouble = myInt;
+
+        if (myAge <= votingAge)
+        {
+            Console.WriteLine("Old enough to vote!");
+        }
+        else
+        {
+            Console.WriteLine("Not old enough to vote.");
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+        }
+    }
+}
+
+
+
+
+
+
+
