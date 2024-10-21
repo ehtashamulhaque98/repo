0052978cb5f1295c43c774c2e90b350c39d364f7
ehtashamul98 <162790649+ehtashamul98@users.noreply.github.com>
2024-10-21 10:36:56 +0530

    web api

---
+++ Web Api/ECommerce/ECommerce/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Models
+{
+    public class Product
+    {
+        public int Product_id { get; set; }
+        public string Product_name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+}
