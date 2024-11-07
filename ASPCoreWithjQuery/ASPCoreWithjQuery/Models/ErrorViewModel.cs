c2f0eb8e0bcf2060b2deffbcddf6653c98e37931
ehtashamul98 <162790649+ehtashamul98@users.noreply.github.com>
2024-11-07 13:24:35 +0530

    Asp

--- ASPCoreWithjQuery/ASPCoreWithjQuery/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPCoreWithjQuery.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
